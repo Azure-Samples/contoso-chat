bd594adcbbe77a7be30e433eefa7c74f326a595b
Cassie Breviu <46505951+cassiebreviu@users.noreply.github.com>
2024-02-12 18:08:15 -0600

    Add deployment of pf to provision.sh

--- provision.sh
+++ provision.sh
@@ -1,5 +1,7 @@
-resourceGroupName="contchat-rg"
+resourceGroupName="testcontchat-rg"
 resourceGroupLocation="swedencentral"
+endpointName="contoso-chat-store"
+deploymentName="contoso-chat"
 
 if [ -z "$(az account show)" ]; then
     echo "You are not logged in. Please run 'az login' or 'az login --use-device-code' first."
@@ -49,5 +51,31 @@ echo "COSMOS_KEY=$cosmosKey" >> .env
 echo "Writing config.json file for PromptFlow usage..."
 subscriptionId=$(az account show --query id -o tsv)
 echo "{\"subscription_id\": \"$subscriptionId\", \"resource_group\": \"$resourceGroupName\", \"workspace_name\": \"$mlProjectName\"}" > config.json
+
+# Deploy prompt flow
+
+az extension add -n ml -y
+echo "HASH=$(echo -n $RANDOM | sha1sum | cut -c 1-6)" >> "$GITHUB_ENV"
+echo "deploymentName=$(echo 'contoso-chat-'$HASH)" >> "$GITHUB_ENV"
+az ml online-endpoint create --file deployment/chat-endpoint.yaml -n $endpointName -g $resourceGroupName -w $mlProjectName
+# Update deployment PRT_CONFIG variable
+PRT_CONFIG_OVERRIDE=deployment.subscription_id=$subscription_id,deployment.resource_group=$resourceGroupName,deployment.workspace_name=$mlProjectName,deployment.endpoint_name=$endpointName,deployment.deployment_name=$deploymentName
+sed -i "s/PRT_CONFIG_OVERRIDE:.*/PRT_CONFIG_OVERRIDE: $PRT_CONFIG_OVERRIDE/g" deployment/chat-deployment.yaml
+# Setup deployment
+az ml online-deployment create --file deployment/chat-deployment.yaml --name $deploymentName --endpoint-name $endpointName --all-traffic -g $resourceGroupName -w $mlProjectName
+az ml online-endpoint show -n $endpointName -g $resourceGroupName -w $mlProjectName
+az ml online-deployment get-logs --name $deploymentName --endpoint-name $endpointName -g $resourceGroupName -w $mlProjectName
+# Read endpoint principal
+az ml online-endpoint show -n $endpointName -g $resourceGroupName -w $mlProjectName > endpoint.json
+jq -r '.identity.principal_id' endpoint.json > principal.txt
+echo "Principal is: $(cat principal.txt)"
+#Assign Permission to Endpoint Principal
+az role assignment create --assignee $(cat principal.txt) --role "AzureML Data Scientist" --scope "/subscriptions/$subscription_id/resourcegroups/$resourceGroupName/providers/Microsoft.MachineLearningServices/workspaces/$mlProjectName"
+az role assignment create --assignee $(cat principal.txt) --role "Azure Machine Learning Workspace Connection Secrets Reader" --scope "/subscriptions/$subscription_id/resourcegroups/$resourceGroupName/providers/Microsoft.MachineLearningServices/workspaces/$mlProjectName/onlineEndpoints/$endpointName"
+# Get keyValueName from Azure ML
+keyValueName=$(az ml online-endpoint show -n $endpointName -g $resourceGroupName -w $mlProjectName --query "identity.principal_id" -o tsv)          
+echo "assigning permissions to Principal to Key vault.."
+az keyvault set-policy --name $keyValueName --resource-group $resourceGroupName --object-id $(cat principal.txt) --secret-permissions get list
+    
 
 echo "Provisioning complete!"
