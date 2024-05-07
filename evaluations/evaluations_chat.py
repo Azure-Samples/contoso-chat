a6c8746a744df3b73fcc13c31a6baa4f6b5685da
Jessica Deen <jldeen@github.com>
2024-05-07 12:37:33 -0700

    WIP evals

--- evaluations/evaluations_chat.py
+++ evaluations/evaluations_chat.py
@@ -1,73 +1,73 @@
 import os
 import sys
-
-
-script_dir = os.path.dirname(os.path.realpath(__file__))
-
-
-contoso_chat_dir = os.path.join(script_dir, '../contoso_chat')
-
-
-sys.path.append(contoso_chat_dir)
-
-
-from chat_request import get_response
-
-
 import json
-
 import pandas as pd
-
 from promptflow.core import AzureOpenAIModelConfiguration
-
 from promptflow.evals.evaluators import RelevanceEvaluator, GroundednessEvaluator, FluencyEvaluator, CoherenceEvaluator
-
 from promptflow.evals.evaluate import evaluate
 
-# Initialize Azure OpenAI Connection
-model_config = AzureOpenAIModelConfiguration(
-        azure_deployment="gpt-4",
-        api_key=os.environ["AZURE_OPENAI_API_KEY"],
-        api_version=os.environ["AZURE_OPENAI_API_VERSION"],
-        azure_endpoint=os.environ["AZURE_OPENAI_ENDPOINT"]
-    )
+sys.path.append('../contoso_chat')
+from chat_request import get_response
 
-data_path = "jldeen/data.jsonl"
+if __name__ == '__main__':
+    # Initialize Azure OpenAI Connection
+    model_config = AzureOpenAIModelConfiguration(
+            azure_deployment="gpt-4",
+            api_key=os.environ["AZURE_OPENAI_API_KEY"],
+            api_version=os.environ["AZURE_OPENAI_API_VERSION"],
+            azure_endpoint=os.environ["AZURE_OPENAI_ENDPOINT"]
+        )
+    
+    # set the path to the data file to use for evaluations
+    data_path = "../data/data.jsonl"
 
-# Check if the file exists and is not empty
-if os.path.isfile(data_path) and os.path.getsize(data_path) > 0:
-    df = pd.read_json(data_path, lines=True)
-    df.head()
-else:
-    print(f"No data found at {data_path}")
+    # Check if the file exists and is not empty
+    if os.path.isfile(data_path) and os.path.getsize(data_path) > 0:
+        # Read the JSON lines file into a pandas DataFrame
+        df = pd.read_json(data_path, lines=True)
+        df.head()
+    else:
+        print(f"No data found at {data_path}")
 
-relevance_evaluator = RelevanceEvaluator(model_config)
-groundedness_evaluator = GroundednessEvaluator(model_config)
-fluency_evaluator = FluencyEvaluator(model_config)
-coherence_evaluator = CoherenceEvaluator(model_config)
+    # Create evaluators for different evaluation metrics
+    relevance_evaluator = RelevanceEvaluator(model_config)
+    groundedness_evaluator = GroundednessEvaluator(model_config)
+    fluency_evaluator = FluencyEvaluator(model_config)
+    coherence_evaluator = CoherenceEvaluator(model_config)
 
-result_eval = evaluate(
-    data="jldeen/data.jsonl",
-    target=get_response,
-    evaluators={
-        #"violence": violence_eval,
-        "relevance": relevance_evaluator,
-        "fluency": fluency_evaluator,
-        "coherence": coherence_evaluator,
-        "groundedness": groundedness_evaluator,
-    },
-    # column mapping    return {"question": question, "answer": result, "context": context}
-    evaluator_config={
-        "defaultS": {
-            "question": "${data.question}",
-            "answer": "${target.question}",
-            "context": "${target.chat_history}",
+    # Perform evaluation using the evaluate function
+    result_eval = evaluate(
+        data="../data/data.jsonl",
+        target=get_response,
+        evaluators={
+            "relevance": relevance_evaluator,
+            "fluency": fluency_evaluator,
+            "coherence": coherence_evaluator,
+            "groundedness": groundedness_evaluator,
         },
-    },
-)
+        evaluator_config={
+            "defaultS": {
+                "question": "${data.question}",
+                "answer": "${target.question}",
+                "context": "${target.chat_history}",
+            },
+        },
+    )
+    
+    # Print result_eval to json file
+    with open('result_eval.json', 'w') as f:
+        json.dump(result_eval, f) 
 
-eval_result = pd.DataFrame(result_eval["rows"])
+    # Convert the evaluation results to a pandas DataFrame
+    eval_result = pd.DataFrame(result_eval["rows"])
+    
+    fmtresult = eval_result.drop(['outputs.context', 'outputs.answer', 'inputs.customerId', 'inputs.chat_history', 'inputs.intent', 'line_number'], axis=1)
 
-eval_result.head()
-
-eval_result.to_json('eval_result.jsonl', orient='records', lines=True)
+    # Save the evaluation results as JSON lines and Markdown files
+    eval_result.to_json('eval_result.jsonl', orient='records', lines=True)
+    
+    # Create headers for our markdown table
+    headers = ["ID", "Question", "Relevance", "Fluency", "Coherence", "Groundedness"]
+    
+    # Print the formatted evaluation results
+    fmtresult.to_markdown('eval_result.md', headers=headers)
