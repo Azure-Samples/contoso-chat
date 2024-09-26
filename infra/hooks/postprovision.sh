a29f478c6c593953d4c116ad64fef427e7366526
sudivate <sudivate@microsoft.com>
2024-09-26 21:24:12 +0000

    update post deployment hooks
    Signed-off-by: sudivate <sudivate@microsoft.com>

--- infra/hooks/postprovision.sh
+++ infra/hooks/postprovision.sh
@@ -34,18 +34,18 @@ azd env get-values >.env
 
 echo "--- ✅ | 1. Post-provisioning - env configured ---"
 
-# # Setup to run notebooks
-# echo 'Installing dependencies from "requirements.txt"'
-# python -m pip install -r ./src/api/requirements.txt > /dev/null
-# python -m pip install ipython ipykernel > /dev/null      # Install ipython and ipykernel
-# ipython kernel install --name=python3 --user > /dev/null # Configure the IPython kernel
-# jupyter kernelspec list > /dev/null                      # Verify kernelspec list isn't empty
-# echo "--- ✅ | 2. Post-provisioning - ready execute notebooks ---"
+# Setup to run notebooks
+echo 'Installing dependencies from "requirements.txt"'
+python -m pip install -r ./src/api/requirements.txt > /dev/null
+python -m pip install ipython ipykernel > /dev/null      # Install ipython and ipykernel
+ipython kernel install --name=python3 --user > /dev/null # Configure the IPython kernel
+jupyter kernelspec list > /dev/null                      # Verify kernelspec list isn't empty
+echo "--- ✅ | 2. Post-provisioning - ready execute notebooks ---"
 
-# echo "Populating data ...."
-# jupyter nbconvert --execute --to python --ExecutePreprocessor.timeout=-1 data/customer_info/create-cosmos-db.ipynb > /dev/null
-# jupyter nbconvert --execute --to python --ExecutePreprocessor.timeout=-1 data/product_info/create-azure-search.ipynb > /dev/null
-# echo "--- ✅ | 3. Post-provisioning - populated data ---"
+echo "Populating data ...."
+jupyter nbconvert --execute --to python --ExecutePreprocessor.timeout=-1 data/customer_info/create-cosmos-db.ipynb > /dev/null
+jupyter nbconvert --execute --to python --ExecutePreprocessor.timeout=-1 data/product_info/create-azure-search.ipynb > /dev/null
+echo "--- ✅ | 3. Post-provisioning - populated data ---"
 
 #echo "Running evaluations ...."
 #jupyter nbconvert --execute --to python --ExecutePreprocessor.timeout=-1 evaluations/evaluate-chat-flow-sdk.ipynb
@@ -55,6 +55,9 @@ echo "--- ✅ | 1. Post-provisioning - env configured ---"
 
 
 echo  "Building contosochatweb:latest..."
+echo -e "\e[31mWarning: Building Frotend Image take a while, please be patient\e[0m"
+echo -e "\e[33Alternatively you can skip this step and build it manually\e[0m"
+
 az acr build --subscription ${AZURE_SUBSCRIPTION_ID} --registry ${AZURE_CONTAINER_REGISTRY_NAME} --image contosochatweb:latest ./src/web/
 web_image_name="${AZURE_CONTAINER_REGISTRY_NAME}.azurecr.io/contosochatweb:latest"
 az containerapp update --subscription ${AZURE_SUBSCRIPTION_ID} --name ${WEBAPP_ACA_NAME} --resource-group ${AZURE_RESOURCE_GROUP} --image ${web_image_name}
