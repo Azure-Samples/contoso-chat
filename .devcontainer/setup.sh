ad2fc12d1dfbfab33c041f1f629e0e404f4f32a0
Nitya Narasimhan <nitya@users.noreply.github.com>
2024-11-05 15:59:38 +0000

    update devcontainer.json

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Install NPM modules for Contoso Web UI
+echo "Installing NPM modules for Contoso Web UI"
+# pushd ./src/web
+# npm install
+# npx next telemetry disable
+# popd
